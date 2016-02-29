c4d2e805d03781ac4e03f2ff925c1142f67269d6
Nipel-Crumple <varnavskii@phystech.edu>
2016-03-01 01:48:33 +0300

    telegram-bot skeleton was added.
    Review feedback

--- spend_ya_project/config.example.py
+++ spend_ya_project/config.example.py
@@ -7,3 +7,6 @@ DATABASE_URL = 'postgres://USER:LOGIN@HOST:PORT/DB_NAME'
 # Yandex money client_id and redirect_uri
 MONEY_CLIENT_ID = 'CLIENT_ID'
 MONEY_REDIRECT_URI = 'REDIRECT_URI'
+
+# Telegram bot API
+TELEGRAM_TOKEN_ID = 'TOKEN_ID'
--- spend_ya_telegram/views.py
+++ spend_ya_telegram/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import emoji
+import telepot
 
-# Create your views here.
+try:
+    from spend_ya_project.config import TELEGRAM_TOKEN_ID
+except ImportError:
+    import os
+    TELEGRAM_TOKEN_ID = os.environ.get('TELEGRAM_TOKEN_ID')
+
+
+class SpendYaBot(telepot.Bot):
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == 'text':
+            msg_text = msg.get('text')
+            user_from = msg.get('from')
+            username = user_from.get('username')
+            if msg_text.startswith('/start'):
+                self.sendMessage(chat_id, text=(emoji.emojize(
+                    'Hello, {0}! Let\'s start working together!:grinning:'.format(username), use_aliases=True)))
+            elif msg_text.startswith('/help'):
+                self.sendMessage(chat_id, text='Trust me, I\'m an Engineer!')
+            else:
+                self.sendMessage(chat_id, text='Not quite my Tempo')
+
+    def on_inline_query(self, msg):
+        # for creating answers
+        print('Inline MSG: ', msg)
+
+    def on_chosen_inline_result(self, msg):
+        # after user chose result from inline query
+        print('Chosen Inline Result MSG: ', msg)
+
+bot = SpendYaBot(TELEGRAM_TOKEN_ID)
+bot.notifyOnMessage()
+print('Bot started working...')
